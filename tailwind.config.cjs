a6520617a95036cdf72a62d5fbbe9b0f92bf3ae0
Laura <lafa0001@stud.kea.dk>
2024-04-29 22:15:23 +0200

    første del

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-    plugins: [
-        require('@tailwindcss/typography'),
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/aspect-ratio'),
-        require('@tailwindcss/container-queries'),
-    ],
-  };
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms"), require("@tailwindcss/aspect-ratio"), require("@tailwindcss/container-queries")],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      yellow: "#FFE000",
+      blue: "#196572",
+    },
+    extend: {
+      fontFamily: {
+        Prompt: ["Prompt", "sans-serif"],
+      },
+    },
+  },
+
+  plugins: [],
+};
